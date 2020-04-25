723bcc7e1f82ba48b4824f496c03c0d0e0edc96f
Marcos Nery <marcosnery.comp@gmail.com>
2020-04-25 06:51:20 -0300

    Setting up initial web layout and displaying scrapping results with ScrapyRT

--- web/app.py
+++ web/app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import itertools
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def homee():
+def index():
 
-    return "Hello World"
- 
+    respG1 = requests.get(
+        url="http://localhost:9080/crawl.json?start_requests=true&spider_name=g1"
+    ).json()
+
+    respTerra = requests.get(
+        url="http://localhost:9080/crawl.json?start_requests=true&spider_name=terra"
+    ).json()
+
+    items = filter(None, sum(itertools.zip_longest(
+        respG1.get('items'), respTerra.get('items')), ()))
+
+    return render_template('index.html', news=items)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
