4a1b7a3968abfe338e80b718f8fd97908f26e8d1
Marcos Nery <marcosnery.comp@gmail.com>
2020-04-26 04:49:01 -0300

    Adding logic to change scrapped news based on selected region

--- web/app.py
+++ web/app.py
@@ -1,25 +1,31 @@
 from flask import Flask, render_template
 import requests
 import itertools
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/<string:region>', methods=['GET'])
+def index(region):
 
     respG1 = requests.get(
-        url="http://localhost:9080/crawl.json?start_requests=true&spider_name=g1"
+        url=f"http://localhost:9080/crawl.json?start_requests=true&spider_name=g1&region={region}"
     ).json()
 
     respTerra = requests.get(
-        url="http://localhost:9080/crawl.json?start_requests=true&spider_name=terra"
+        url=f"http://localhost:9080/crawl.json?start_requests=true&spider_name=terra&region={region}"
     ).json()
 
     items = filter(None, sum(itertools.zip_longest(
         respG1.get('items'), respTerra.get('items')), ()))
 
-    return render_template('index.html', news=items)
+    regions = {"AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG",
+               "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}
+
+    return render_template('index.html', news=items, regions=regions)
 
 
 if __name__ == '__main__':
