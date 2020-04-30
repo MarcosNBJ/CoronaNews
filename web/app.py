de9436a6318648d849000cc154a994b1255465c7
Marcos Nery <marcosnery.comp@gmail.com>
2020-04-30 04:48:15 -0300

    Changing Flask's frontend code to grab scrapy's url form env variable and improving code comments

--- web/app.py
+++ web/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 import itertools
 from flask_bootstrap import Bootstrap
+import os
 
 
 app = Flask(__name__)
@@ -11,21 +12,31 @@ bootstrap = Bootstrap(app)
 @app.route('/', defaults={'region': ''})
 @app.route('/<string:region>', methods=['GET'])
 def index(region):
+    '''
+    Endpoint that responsible for sending a request to the scrapyrt endpoint and displaying 
+    the response in a prettier frontend.
+
+    '''
+    # environment variable containing scrapyrt's endpoint URL
+    scrapyrt_url = os.environ.get("SCRAPY_URL")
 
     respG1 = requests.get(
-        url=f"http://localhost:9080/crawl.json?start_requests=true&spider_name=g1&region={region}"
+        url=f"{scrapyrt_url}/crawl.json?start_requests=true&spider_name=g1&region={region}"
     ).json()
 
     respTerra = requests.get(
-        url=f"http://localhost:9080/crawl.json?start_requests=true&spider_name=terra&region={region}"
+        url=f"{scrapyrt_url}/crawl.json?start_requests=true&spider_name=terra&region={region}"
     ).json()
 
+    # making a list that contains both responses merged in order
     items = filter(None, sum(itertools.zip_longest(
         respG1.get('items'), respTerra.get('items')), ()))
 
+    # list containing all brazillian states, to be displayed in the options button
     regions = {"AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG",
                "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}
 
+    # passing both lists to template
     return render_template('index.html', news=items, regions=regions)
 
 
